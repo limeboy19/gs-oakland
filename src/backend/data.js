52ec63a299b9bdead93eb429dd8ded55f53516a0
limeboy19 <emil@actonemedia.com>
2025-03-30 18:11:30 -0400

    Remove wix-logging (not supported in Wix Studio); use console.log instead

--- src/backend/data.js
+++ src/backend/data.js
@@ -1,18 +1,18 @@
 import wixData from 'wix-data';
 import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/RichContent.js';
-import { log } from 'wix-logging';
+
 
 let authOptions = { suppressAuth: true };
 
 export async function RichContent_afterInsert(item, context, authOptions) {
-  log.info("afterInsert_RichContent triggered", item );
+  console.log("afterInsert_RichContent triggered", item );
   console.log("are we hitting this?");
   afterInsertRichContent(fullItem, context, authOptions);
 
 }
 
 export async function RichContent_afterUpdate(item, context, authOptions) {
-  log.info("afterUpdate_RichContent triggered", { data: item });
+  console.log("afterUpdate_RichContent triggered", { data: item });
   afterUpdateRichContent(fullItem, authOptions);
   
 }
