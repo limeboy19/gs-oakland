a4a318e7d83252f9e826db9b0380683cb7d3de7d
limeboy19 <emil@actonemedia.com>
2025-03-30 18:17:33 -0400

    Clean up afterInsert/afterUpdate hooks: remove unused context, add await, return item

--- src/backend/data.js
+++ src/backend/data.js
@@ -4,15 +4,22 @@ import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/Ri
 
 let authOptions = { suppressAuth: true };
 
-export async function RichContent_afterInsert(item, context, authOptions) {
-  console.log("afterInsert_RichContent triggered", item );
-  console.log("are we hitting this?");
-  afterInsertRichContent(fullItem, context, authOptions);
+export async function afterInsert_RichContent(item) {
+  console.log("afterInsert_RichContent triggered", item);
 
+  const fullItem = await wixData.get("RichContent", item._id, authOptions);
+
+  try {
+    await afterInsertRichContent(fullItem);
+  } catch (err) {
+    console.error("Error in afterInsertRichContent:", err);
+  }
+
+  return fullItem;
 }
 
 export async function RichContent_afterUpdate(item, context, authOptions) {
   console.log("afterUpdate_RichContent triggered", { data: item });
-  afterUpdateRichContent(fullItem, authOptions);
+  await afterUpdateRichContent(fullItem, authOptions);
   
 }
