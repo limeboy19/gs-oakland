162a4e9d18b9013510c3b3c104911dfeba3955f7
limeboy19 <emil@actonemedia.com>
2025-03-30 16:07:42 -0400

    Creating Hooks

---
+++ src/backend/data.js
@@ -0,0 +1,6 @@
+// backend/data.js
+import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/RichContent.js';
+import wixDataHooks from 'wix-data';
+
+wixDataHooks.afterInsert("RichContent", afterInsertRichContent);
+wixDataHooks.afterUpdate("RichContent", afterUpdateRichContent);
