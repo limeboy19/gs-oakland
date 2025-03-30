344d4b926139ac042228ffc114b6fe63e2313545
limeboy19 <emil@actonemedia.com>
2025-03-30 17:54:43 -0400

    Fix permission error in RichContent hooks using suppressAuth with get()

--- src/backend/data.js
+++ src/backend/data.js
@@ -2,21 +2,16 @@ import wixData from 'wix-data';
 import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/RichContent.js';
 import { log } from 'wix-logging';
 
-const authOptions = { suppressAuth: true };
+let authOptions = { suppressAuth: true };
 
-export async function afterInsert_RichContent(item, context) {
-  log.info("afterInsert_RichContent triggered", { data: item });
+export async function afterInsert_RichContent(item, context, authOptions) {
+  log.info("afterInsert_RichContent triggered", item );
+  afterInsertRichContent(fullItem, context, authOptions);
 
-
-  const fullItem = await wixData.get("RichContent", item._id, authOptions);
-  await afterInsertRichContent(fullItem, context);
-  return fullItem;
 }
 
-export async function afterUpdate_RichContent(item, context) {
+export async function afterUpdate_RichContent(item, context, authOptions) {
   log.info("afterUpdate_RichContent triggered", { data: item });
-
-  const fullItem = await wixData.get("RichContent", item._id, authOptions);
-  await afterUpdateRichContent(fullItem, context);
-  return fullItem;
+  afterUpdateRichContent(fullItem, context, authOptions);
+  
 }
