2881c67adf17a952a3fff86c19132a841b621913
limeboy19 <emil@actonemedia.com>
2025-03-30 17:25:45 -0400

    Add wix-logging to afterInsert and afterUpdate hooks for visibility

--- src/backend/data.js
+++ src/backend/data.js
@@ -1,12 +1,15 @@
 import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/RichContent.js';
+import { log } from 'wix-logging';
 
 export async function afterInsert_RichContent(item, context) {
+  log.info("afterInsert_RichContent triggered", { data: item });
   console.log("afterInsert_RichContent triggered", item);
   await afterInsertRichContent(item, context);
   return item;
 }
 
 export async function afterUpdate_RichContent(item, context) {
+  log.info("afterUpdate_RichContent triggered", { data: item });
   console.log("afterUpdate_RichContent triggered", item);
   await afterUpdateRichContent(item, context);
   return item;
