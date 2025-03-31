7dee8ad4339785ea4fe82063740802b0bb2b8f63
limeboy19 <emil@actonemedia.com>
2025-03-30 20:54:42 -0400

    Fix afterUpdate_RichContent: unwrap item.data for proper field access

--- src/backend/data.js
+++ src/backend/data.js
@@ -5,14 +5,13 @@ import { afterInsertRichContent, afterUpdateRichContent } from 'backend/hooks/Ri
 let authOptions = { suppressAuth: true };
 
 export function RichContent_afterInsert(item, context) {
-  console.log("Emil testing");
   console.log("afterInsert_RichContent triggered", item);
   afterInsertRichContent(item,authOptions);
   
 }
 
 export async function RichContent_afterUpdate(item, context) {
-  console.log("afterUpdate_RichContent triggered", { data: item });
-  await afterUpdateRichContent(fullItem, authOptions);
+  console.log("afterUpdate_RichContent triggered", item);
+  afterUpdateRichContent(item, authOptions);
   
 }
