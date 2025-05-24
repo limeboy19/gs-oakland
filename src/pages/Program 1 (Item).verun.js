723f97c3ae02ba8e98c2a11270583955bf159863
limeboy19 <emil@actonemedia.com>
2025-05-24 16:42:03 +0200

    Adjust program 1 page for null values check

--- src/pages/Program 1 (Item).verun.js
+++ src/pages/Program 1 (Item).verun.js
@@ -1,6 +1,52 @@
+//Wix
+import wixLocation from 'wix-location';
+import wixData from 'wix-data';
+
+//Helper functions
 import { handleNext, handleBack } from 'public/arrows.web.js';
 
-$w.onReady(function () {
+$w.onReady(async function () {
+
+    const fullUrl = wixLocation.url;
+    console.log("Full URL:", fullUrl);
+
+    const currentItem = $w("#dsPrograms").getCurrentItem();
+    const programId = currentItem._id;
+
+    let fullItem;
+
+    try {
+        const { items } = await wixData.query("Programs")
+            .eq("_id", programId)
+            .include("ProgramFAQ_programs")
+            .include("HowWeSupportYourFamily_programs")
+            .include("Showcase_programs")
+            .find();
+
+        fullItem = items[0];
+        //console.log("Full item with multi-ref fields:", fullItem);
+    } catch (error) {
+        console.error("Error querying multi-ref fields:", error);
+    }
+
+    if (fullItem?.ProgramFAQ_programs?.length < 1) {
+        $w("#sectionFAQ").collapse();
+        $w("#sectionFAQ2").collapse();
+        
+    }
+
+    if (fullItem?.HowWeSupportYourFamily_programs?.length < 1) {
+        $w("#sectionVideo").collapse();
+    }
+
+    if (fullItem?.Showcase_programs?.length < 1) {
+        $w("#sectionTestimonials").collapse();
+    }
+
+    if(!fullItem?.getInTouchLink){
+        $w("#btnGetInTouch").collapse();
+    }
+
     $w("#repeaterQuestions").onItemReady(($item, itemData) => {
         const textBox = $item("#repeaterAnswer");
         const textTitleButton = $item("#btnRepeaterHideShow");
